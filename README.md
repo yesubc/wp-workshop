70594124978709f52a03060d5cbddbba1e9e6575
Sunil Shrestha <sshrestha@lftechnology.com>
2015-09-01 15:22:55 +0545

    Sass and Less implement.

--- README.md
+++ README.md
@@ -2,9 +2,9 @@ WordPress Workshop
 ===================
     
 #### Configure WordPress:
-1. Pull in your local 'git clone git@github.com:yesubc/wp-workshop.git'
-2. Import database from 'Data' folder
-3. Access to Admin 'http://localhost/wp-workshop/wp-admin'
+1. Pull in your local "git clone git@github.com:yesubc/wp-workshop.git"
+2. Import database from "Data" folder
+3. Access to Admin "http://localhost/wp-workshop/wp-admin"
 4. Admin authentication
     Username: admin
     Password: 111111
@@ -16,13 +16,15 @@ WordPress Workshop
 #### Install server-side dependencies
     $ npm install
     
-#### Run Gulp tasks
+#### Run Gulp Tasks:
     $ gulp
     $ gulp watch
 
-#### Reference
+#### Reference:
     https://github.com/yesubc/gulpjs-bower
     
 ##### Note: 
-1. Change http://localhost/wp-workshop/ to your database table of mytheme_options
-2. You need to install 'nodejs', 'npm' if you don't have them already.
+1. Change http://localhost/wp-workshop/ to your database table of mytheme_options.
+2. Gulp and NPM install in your theme location i.e. wp-content\themes\mytheme.
+3. You need to install 'nodejs', 'npm' if you don't have them already.
+4. Less automation; Go to "gulpfile.js" and enable to "Less" and disable to "SASS".
--- wp-content/themes/mytheme/gulpfile.js
+++ wp-content/themes/mytheme/gulpfile.js
@@ -5,27 +5,43 @@
     var gulp        = require('gulp'),
         wrap        = require('gulp-wrap'),
         sass        = require('gulp-sass'),
+        //less        = require('gulp-less'),
         watch       = require('gulp-watch'),
         concat      = require('gulp-concat'),
         notify      = require("gulp-notify"),
         declare     = require('gulp-declare')
+        //Less
+        //path        = require('path');
 
     // Source Path
-    var SOURCE_SASS = 'sass/**/*.scss';
+    var SOURCE_SASS = 'assets/sass/**/*.scss';
+    //var SOURCE_LESS = 'assets/less/**/*.less';
 
     // Sass compilation
     gulp.task('sass', function () {
         gulp.src(SOURCE_SASS)
             .pipe(sass().on('error', sass.logError))
-            .pipe(gulp.dest('css'))
+            .pipe(gulp.dest('assets/css'))
             .pipe(notify('Sass Compiled'));
     });
+
+    // Less Compilation
+    //gulp.task('less', function () {
+      //return gulp.src('assets/less/**/*.less')
+        //.pipe(less({
+          //paths: [ path.join(__dirname, 'less', 'includes') ]
+        //}))
+        //.pipe(gulp.dest('assets/css'))
+        //.pipe(notify('Less Compiled'));
+    //});
 
     // Watch task
     gulp.task('watch', function () {
         gulp.watch(SOURCE_SASS, ['sass']);
+        //gulp.watch(SOURCE_LESS, ['less']);
     });
 
-    gulp.task('default', ['sass']);    
+    gulp.task('default', ['sass']);
+    //gulp.task('default', ['less']);  
 
 })();
